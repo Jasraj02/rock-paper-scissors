426ee821de5404549a0f4df8dd9b29ba5b9262ad
Jasraj Aulakh <177463743+Jasraj02@users.noreply.github.com>
2024-08-15 16:14:07 +0100

    Make random integer function

---
+++ rps-game.js
@@ -0,0 +1,15 @@
+
+console.log("Hello World!")
+
+// Computer Choice
+// function will return rock, paper or scissorts
+// create a function that returns random ints (0,1,2)
+// 
+
+let randomChoice = (num=2) => Math.round(Math.random() * num);
+
+console.log(randomChoice());
+
+
+function getComputerChoice() {};
+
