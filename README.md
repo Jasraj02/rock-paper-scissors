c63addb77d79e4da2aa1e280d80abebf39a1f79e
Jasraj Aulakh <177463743+Jasraj02@users.noreply.github.com>
2024-08-15 18:51:40 +0100

    Add readme message

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rock-paper-scissors
+
+Basic rock paper scissors game made in JavaScript.
+Meant to be played via the console. 
--- rps-game.js
+++ rps-game.js
@@ -1,8 +1,5 @@
 
 
-
-
-
 // create a function that returns random ints (0,1,2)
 let randomChoice = (num=2) => Math.round(Math.random() * num);
 
