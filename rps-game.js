df40bd985819840ca5337f732bb92e993e82584a
Jasraj Aulakh <177463743+Jasraj02@users.noreply.github.com>
2024-08-15 17:19:19 +0100

    Implement logic for each round with text outputs
    Logic for each indiviudal round of the game is
    implemented, along with text outputs printed
    to the console for the user to see if they won,
    lost or tied the round

--- rps-game.js
+++ rps-game.js
@@ -4,25 +4,82 @@ console.log("Hello World!")
 // Computer Choice
 // function will return rock, paper or scissorts
 // create a function that returns random ints (0,1,2)
-// 
+
 
 let randomChoice = (num=2) => Math.round(Math.random() * num);
 
-var choice;
 
 function getComputerChoice() {
+    var comChoice;
     switch(randomChoice()) {
         case 0:
-            choice = "rock";
+            comChoice = "rock";
             break;
         case 1:
-            choice = "paper";
+            comChoice = "paper";
             break;
         case 2:
-            choice = "scissors";
+            comChoice = "scissors";
             break;
     }
+    return comChoice;
 };
 
-getComputerChoice();
-console.log(choice);
+
+// Human Choice 
+// use prompt method 
+// take string input 
+
+function getHumanChoice() {
+    var humChoice = prompt("Type Rock, Paper or Scissors: ")
+    humChoice = humChoice.toLowerCase()
+    return humChoice;
+};
+
+
+// Variables that keep track of Human and Computer scores
+var humanScore = 0;
+var computerScore = 0;
+
+// Functions to create win/loss message for a round
+let winMessage = (humChoice,compChoice) => {
+    console.log(`You win! ${humChoice} beats ${compChoice}`);
+};
+let lossMessage = (humChoice,compChoice) => {
+    console.log(`You lose! ${compChoice} beats ${humChoice}`);
+};
+
+
+// Logic to play a single round
+// use if, elif and else within a function
+// finish function with a win/loss/tie message 
+
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`You both chose ${humanChoice}! This round is a tie.`)
+    }
+
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        winMessage(humanChoice,computerChoice);
+    }
+
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        winMessage(humanChoice,computerChoice);
+    }
+
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        winMessage(humanChoice,computerChoice);
+    }
+
+    else {
+        lossMessage(humanChoice,computerChoice);
+    }
+
+};
+
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice); // remove this line once finished
+const humanChoice = getHumanChoice();
+
+playRound(humanChoice,computerChoice);
